b638fe3903d0feb9529bcce8007bfa816564edc1
Mohamed Raslan <mohumedraslan@gmail.com>
2025-03-28 10:06:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1jJmXxHg0oljCRyKF4qTi9iU-JJqQ4SMd
 """
 
-!pip install streamlit
 
 from google.colab import files
 files = files.upload()
